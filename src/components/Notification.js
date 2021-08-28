5c54ddffd758e8f5b177e19b4cd4d563c9deaef6
Cody Torno <ctorno3@gmail.com>
2021-08-28 14:22:50 -0500

    Added Styled Components package
    - found an issue in production build where notifications were not coming
    in correctly
    - this issue does not present itself in dev testing..
    - added a styled component for notification so styling lives inside same
    location as notification component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,10 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const NotificationWrapper = styled.div`
+  background: lightgray;
+  position: absolute;
+  color: green;
+  bottom: 10px;
+  left: 10px;
+  right: 10px;
+  padding: 5px;
+  border: 3px solid;
+  border-radius: 3px;
+  border-color: green;
+`;
 
 const Notification = ({ message }) => {
   if (!message) {
     return null;
   }
-  return <div className="notification">{message}</div>;
+  return <NotificationWrapper>{message}</NotificationWrapper>;
 };
 
 export default Notification;
